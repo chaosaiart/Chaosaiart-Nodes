498c97bca146676d0383c14b5aaa87b25a1a31c2
Chaos AI Art <git_chaosaiart@dane-blauert.de>
2024-04-18 14:38:07 +0200

    -

--- __init__.py
+++ __init__.py
@@ -1,4 +1,17 @@
 from .nodes.nodes import NODE_CLASS_MAPPINGS,NODE_DISPLAY_NAME_MAPPINGS
 
+# Gelber Text
+#print("🔶"+"\033[33mThis is an example in yellow.\033[0m")
+# Orangefarbener Text
+#print("🔶"+"\033[38;5;208mThis is an example in orange.\033[0m")
+#print("🔶"+"\033[38;5;220mThis is an example in gold.\033[0m")
 
+# Helles Gelb
+text = "Chaosaiart: visit our Discord. https://chaosaiart.com/discord"
+padding = (80 - len(text)) // 2  # Berechne den Abstand für die Mitte
+#print("\n\n")
+#print("🔶"+"\033[38;5;229mChaosaiart : visit your Discord. https://chaosaiart.com/discord\033[0m")
+print("\n\n")
+print(" " * padding +"🔶"+"\033[38;5;229m" + text + "\033[0m")
+print("\n\n")
 WEB_DIRECTORY = "./web"
